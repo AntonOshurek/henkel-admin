fd42845287d8e8e1c67edd5ffaf1caf1c5de699f
AntonOshurek <oshurekweb@gmail.com>
2022-06-13 21:56:57 +0200

    add swipe function for menu

--- source/scripts/modules/menu.js
+++ source/scripts/modules/menu.js
@@ -90,4 +90,56 @@ function menuFocus(e) {
   }
 }
 
+  //swips
+  let touchStart = null; //Точка начала касания
+  let touchPosition = null; //Текущая позиция
+  //Чувствительность — количество пикселей, после которого жест будет считаться свайпом
+  const sensitivity = 200;
+
+  bodyElement.addEventListener("touchstart", function (e) { TouchStart(e); }); //Начало касания
+  bodyElement.addEventListener("touchmove", function (e) { TouchMove(e); }); //Движение пальцем по экрану
+  //Пользователь отпустил экран
+  bodyElement.addEventListener("touchend", function (e) { TouchEnd(e); });
+  //Отмена касания
+  bodyElement.addEventListener("touchcancel", function (e) { TouchEnd(e); });
+
+  function TouchStart(e)  {
+    //Получаем текущую позицию касания
+    touchStart = { x: e.changedTouches[0].clientX, y: e.changedTouches[0].clientY };
+    touchPosition = { x: touchStart.x, y: touchStart.y };
+  }
+
+  function TouchMove(e) {
+    //Получаем новую позицию
+    touchPosition = { x: e.changedTouches[0].clientX, y: e.changedTouches[0].clientY };
+  }
+
+  function TouchEnd(e) {
+    CheckAction(); //Определяем, какой жест совершил пользователь
+    //Очищаем позиции
+    touchStart = null;
+    touchPosition = null;
+  }
+
+  function CheckAction()  {
+    let d = //Получаем расстояния от начальной до конечной точек по обеим осям
+    {
+    x: touchStart.x - touchPosition.x,
+    y: touchStart.y - touchPosition.y
+    };
+
+    if(Math.abs(d.x) > Math.abs(d.y)) { //Проверяем, движение по какой оси было длиннее
+      if(Math.abs(d.x) > sensitivity) { //Проверяем, было ли движение достаточно длинным
+        if(d.x > 0) { //Если значение больше нуля, значит пользователь двигал пальцем справа налево
+          console.log('left')
+
+        }
+        else { //Иначе он двигал им слева направо
+          console.log('right')
+
+        }
+      }
+    }
+  }
+
 export { menu };
