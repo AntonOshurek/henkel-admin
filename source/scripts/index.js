635be1e0d1ac5cb0ef8d36b785cf542faad4f7da
AntonOshurek <oshurekweb@gmail.com>
2022-06-27 23:29:19 +0200

    add popup logick

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -1,6 +1,7 @@
 import { tchemeControl } from './modules/tcheme-control';
 import { menuInit } from './modules/menu';
 import initClock from './modules/clock';
+import popup from './modules/popup';
 
 
 
@@ -10,5 +11,9 @@ window.addEventListener('DOMContentLoaded', () => {
 
   if(document.location.pathname === '/index.html' || document.location.pathname === '/henkel-admin/index.html' || document.location.pathname === '/henkel-admin/' || document.location.pathname === '/') {
     initClock();
+  }
+
+  if(document.location.pathname === '/offer.html' || document.location.pathname === '/henkel-admin/offer.html') {
+    popup();
   }
 });
---
+++ source/scripts/modules/popup.js
@@ -0,0 +1,25 @@
+export default function popup() {
+  const infoControls = document.querySelector('.info-controls');
+  const popup = document.querySelector('.popup');
+  const closePopupButton = document.querySelector('.popup__close-button');
+
+  infoControls.addEventListener('click', (evt) => {
+    if (evt.target.tagName === 'BUTTON') {
+      const controlName = evt.target.getAttribute('data-control');
+
+      if(controlName === 'add-asort') {
+        popup.classList.add('popup--open');
+
+        closePopupButton.addEventListener('click', closePopup);
+      }
+    }
+  });
+
+  function closePopup() {
+    console.log('close');
+
+    popup.classList.remove('popup--open');
+
+    closePopupButton.addEventListener('click', closePopup);
+  }
+}
