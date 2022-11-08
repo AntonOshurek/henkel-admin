09099cc78a8581c09b35f644aafa7b497d9cbc8c
AntonOshurek <83346452+AntonOshurek@users.noreply.github.com>
2022-11-08 18:21:49 +0000

    Deploying to gh-pages from @ AntonOshurek/henkel-admin@063f3d60d190fc2b88e65f3eed99bca86a01de2d 🚀

--- js/bundle.js
+++ js/bundle.js
@@ -192,21 +192,22 @@ function popup() {
   const infoControls = document.querySelector('.info-controls');
   const popup = document.querySelector('.popup');
   const closePopupButton = document.querySelector('.popup__close-button');
+  const body = document.querySelector('.body');
   infoControls.addEventListener('click', evt => {
     if (evt.target.tagName === 'BUTTON') {
       const controlName = evt.target.getAttribute('data-control');
 
       if (controlName === 'add-asort') {
-        popup.classList.remove('popup--close');
-        popup.classList.add('popup--open');
+        popup.classList.remove('out');
+        body.classList.add('body--open-popup');
         closePopupButton.addEventListener('click', closePopup);
       }
     }
   });
 
   function closePopup() {
-    popup.classList.remove('popup--open');
-    popup.classList.add('popup--close');
+    popup.classList.add('out');
+    body.classList.remove('body--open-popup');
     closePopupButton.addEventListener('click', closePopup);
   }
 }
