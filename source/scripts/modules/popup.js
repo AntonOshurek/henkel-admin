887cd1c6d2e2a58faac90e9e1c6c72c8abe488b2
Anton Oshurek <oshurekweb@gmail.com>
2022-11-09 14:37:03 +0100

    rename popup to modal and refactoring modal logick and classes names

--- source/scripts/modules/popup.js
+++ source/scripts/modules/popup.js
@@ -1,7 +1,7 @@
 export default function popup() {
   const infoControls = document.querySelector('.info-controls');
-  const popup = document.querySelector('.popup');
-  const closePopupButton = document.querySelector('.popup__close-button');
+  const popup = document.querySelector('.modal');
+  const closePopupButton = document.querySelector('.modal__close-button');
 
   const body = document.querySelector('.body');
 
@@ -9,10 +9,10 @@ export default function popup() {
     if (evt.target.tagName === 'BUTTON') {
       const controlName = evt.target.getAttribute('data-control');
 
-      if(controlName === 'add-asort') {
+      if(controlName === 'open-modal') {
         popup.classList.remove('out');
         popup.classList.add('open');
-        body.classList.add('body--open-popup')
+        body.classList.add('body--open-modal')
         closePopupButton.addEventListener('click', closePopup);
       }
     }
@@ -21,7 +21,7 @@ export default function popup() {
   function closePopup() {
     popup.classList.add('out');
     popup.classList.remove('open');
-    body.classList.remove('body--open-popup');
+    body.classList.remove('body--open-modal');
     closePopupButton.addEventListener('click', closePopup);
   }
 }
