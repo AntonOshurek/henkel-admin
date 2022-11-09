25c69ba65afaf9445a33052daeea7d28a912d76c
Anton Oshurek <oshurekweb@gmail.com>
2022-11-09 22:17:22 +0100

    add shake animation for submit button when we have a inputs errors

--- source/scripts/modules/required-field.js
+++ source/scripts/modules/required-field.js
@@ -1,24 +1,46 @@
 const requiredField = () => {
   const requiredInputs = document.getElementsByTagName('input');
-  // console.log(requiredInputs);
+  const submitButton = document.querySelector('.info-controls__submit-button');
+
+  let errorsArray = [];
 
   for (let i = 0; i < requiredInputs.length; i++) {
 
     if(requiredInputs[i].getAttribute('required') !== null) {
-      console.log(requiredInputs[i])
-      // console.log(requiredInputs[i].parentElement)
       requiredInputs[i].addEventListener('input', validateValue)
     }
   }
-}
 
-function validateValue(evt) {
-  if(evt.target.value === '') {
-    console.log('required!')
+  submitButton.addEventListener('click', (evt) => {
+    evt.preventDefault();
 
-    this.parentElement.classList.add('edit-field--required');
-  } else {
-    this.parentElement.classList.remove('edit-field--required')
+    if(errorsArray.length > 0) {
+      submitButton.classList.add('info-controls__submit-button--error');
+
+      setTimeout(() => {
+        submitButton.classList.remove('info-controls__submit-button--error');
+      }, 1000);
+    }
+  })
+
+  function validateValue(evt) {
+    const requireFieldName = evt.target.parentElement.querySelector('.edit-field__field-name').innerText;
+    if(evt.target.value === '') {
+
+      if(!errorsArray.includes(requireFieldName)) {
+        errorsArray.push(requireFieldName);
+      }
+
+      this.parentElement.classList.add('edit-field--required');
+    } else {
+
+      if(errorsArray.indexOf(requireFieldName) !== -1) {
+        errorsArray.splice(errorsArray.indexOf(requireFieldName), 1)
+      }
+      this.parentElement.classList.remove('edit-field--required')
+    }
+
+    console.log(errorsArray);
   }
 }
 
